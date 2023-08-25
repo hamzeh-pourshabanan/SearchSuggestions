f8c2361ebc02f774498f2fc0df52a1eb87fdc99c
hamzeh <h.pourshabanan@live.com>
2023-08-25 10:55:52 +0330

    feat: Add handler approach for handling debounce

--- app/src/main/java/com/hamzeh/search/SearchActivity.kt
+++ app/src/main/java/com/hamzeh/search/SearchActivity.kt
@@ -9,6 +9,10 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 class SearchActivity: AppCompatActivity() {
 
+    private val handler = Handler(Looper.getMainLooper())
+    private val delayMillis = 500L
+    private var lastUpdateTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,11 +22,25 @@ class SearchActivity: AppCompatActivity() {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                requestSearchSuggestions(s.toString())
+                val currentTime = System.currentTimeMillis()
+                if (currentTime - lastUpdateTime > delayMillis) {
+                    lastUpdateTime = currentTime
+                    requestSearchSuggestions(s.toString())
+                } else {
+                    handler.removeCallbacksAndMessages(null)
+                    handler.postDelayed({
+                        requestSearchSuggestions(s.toString())
+                    }, delayMillis)
+                }
             }
 
             override fun afterTextChanged(s: Editable) {}
         })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 
     private fun requestSearchSuggestions(query: String) {
